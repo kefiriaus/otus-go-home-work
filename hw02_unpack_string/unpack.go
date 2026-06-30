d00fad283b37229693d77e62851c79f9ebafd0dd
Sergey Golovkin <2213220+kefiriaus@users.noreply.github.com>
2026-06-30 21:23:39 +0300

    HW2 is completed (#2)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,115 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
+
+const EscapeRune = '\\'
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+type Rune struct {
+	r       rune
+	escaped bool
+}
+
+func (p *Rune) Update(r rune, escaped bool) {
+	p.r = r
+	p.escaped = escaped
+}
+
+func (p Rune) IsEmpty() bool {
+	return p.r == 0
+}
+
+func (p Rune) IsLetter() bool {
+	return p.escaped || unicode.IsLetter(p.r) || unicode.IsSpace(p.r) || unicode.IsSymbol(p.r)
+}
+
+func (p Rune) IsDigit() bool {
+	return !p.escaped && unicode.IsDigit(p.r)
+}
+
+func (p Rune) IsPunct() bool {
+	return !p.escaped && p.r != EscapeRune && unicode.IsPunct(p.r)
+}
+
+func (p Rune) IsEscape() bool {
+	return !p.escaped && p.r == EscapeRune
+}
+
+func (p Rune) IsEscaped() bool {
+	return p.escaped
+}
+
+func UnpackRune(prev, current Rune, isCurrentLast bool) (res []rune, escaped bool, err error) {
+	if prev.IsEmpty() && current.IsDigit() {
+		return nil, false, ErrInvalidString
+	}
+	if prev.IsEmpty() && current.IsEscape() {
+		return nil, false, ErrInvalidString
+	}
+	if prev.IsDigit() && current.IsDigit() {
+		return nil, false, ErrInvalidString
+	}
+	if prev.IsEscape() && current.IsLetter() {
+		return nil, false, ErrInvalidString
+	}
+	if prev.IsEscape() && current.IsPunct() {
+		return nil, false, ErrInvalidString
+	}
+
+	switch {
+	case prev.IsEscape() && !current.IsEmpty():
+		res = append(res, current.r)
+		return res, true, nil
+	case prev.IsLetter() && !prev.IsEscaped() && !current.IsDigit():
+		res = append(res, prev.r)
+	case (prev.IsLetter() || prev.IsPunct()) && current.IsDigit():
+		n, err := strconv.Atoi(string(current.r))
+		if err != nil {
+			return nil, false, err
+		}
+		if n > 0 {
+			if prev.IsEscaped() {
+				n--
+			}
+			res = []rune(strings.Repeat(string(prev.r), n))
+		}
+	}
+
+	if isCurrentLast && current.IsLetter() {
+		res = append(res, current.r)
+	}
+
+	return res, escaped, nil
+}
+
+func Unpack(s string) (string, error) {
+	sLen := utf8.RuneCountInString(s)
+	if sLen == 0 {
+		return "", nil
+	}
+
+	var prev, current Rune
+	var isCurrentLast, escaped bool
+	var res []rune
+	var err error
+	var n int
+	ss := strings.Builder{}
+	for _, r := range s {
+		current.Update(r, false)
+		isCurrentLast = n == sLen-1
+		res, escaped, err = UnpackRune(prev, current, isCurrentLast)
+		if err != nil {
+			return "", err
+		}
+		prev.Update(r, escaped)
+		ss.WriteString(string(res))
+		n++
+	}
+	return ss.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:depguard
 )
 
 func TestUnpack(t *testing.T) {
@@ -12,21 +12,27 @@ func TestUnpack(t *testing.T) {
 		input    string
 		expected string
 	}{
+		{input: "", expected: ""},
+		{input: "q", expected: "q"},
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "go🙃2", expected: "go🙃🙃"},
+		{input: "Hello,2 世界" , expected: "Hello,, 世界"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\`, expected: `qwe`},
+		{input: `qwe\0`, expected: `qwe0`},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +42,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\111`, "0", `\`, `qw\ne`, `qw\,`}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
