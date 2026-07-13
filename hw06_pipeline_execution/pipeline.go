6ef909e898b65713cd1f167b23e076683c846b87
Sergey Golovkin <2213220+kefiriaus@users.noreply.github.com>
2026-07-13 17:58:11 +0000

    HW06 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,54 @@ type (
 
 type Stage func(in In) (out Out)
 
+func WrapStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func ExecuteStage(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	stageOut := stage(WrapStage(in, done))
+	go func() {
+		defer close(out)
+		for v := range stageOut {
+			select {
+			case out <- v:
+			case <-done:
+				for range stageOut {
+				}
+				return
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	for _, stage := range stages {
+		in = ExecuteStage(in, done, stage)
+	}
+	return in
 }
