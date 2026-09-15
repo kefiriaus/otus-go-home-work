f4f146fd4eb555c35563d0f44e98225f7782cc60
Sergey Golovkin <2213220+kefiriaus@users.noreply.github.com>
2026-09-15 12:53:10 +0300

    HW06 done (#4)

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,45 @@ type (
 
 type Stage func(in In) (out Out)
 
+func guard(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer func() {
+			close(out)
+			//nolint:revive
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
+
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					return
+				}
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := guard(in, done)
+	for _, stage := range stages {
+		out = guard(stage(out), done)
+	}
+	return out
 }
